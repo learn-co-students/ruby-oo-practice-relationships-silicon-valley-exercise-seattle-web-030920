11e40dc2be7241d3be1a61c04adc64d843970b4b
dizhengcindy <dizhengcindy@gmail.com>
2020-03-13 13:39:31 -0700

    OO Relationships Practice: Silicon Valley

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,32 @@
 class Startup
+    attr_reader :name, :domain, :founder
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(domain,name)
+        @domain = domain
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder)
+        #add .all
+        self.all.find {|startup| startup.founder == founder}
+    end
+
+    def self.domains
+        #select will return the orignal stuff, map can return the mutated version
+        #add .all
+        self.all.map {|startup| startup.domain }
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,14 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+st1 = Startup.new("name1", "founder1", "domain1.com")
+st2 = Startup.new("name2", "founder2", "domain2.com")
+st3 = Startup.new("name3", "founder3", "domain3.com")
 
+st1.name
+
+vc1 = VentureCapitalist.new("vc1", 123456789)
+vc2 = VentureCapitalist.new("vc2", 10000000001)
+vc3 = VentureCapitalist.new("vc3", 10000000002)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
