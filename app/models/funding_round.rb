fe4dbc59c9de4d33d06a5feda32cbce063c97bfe
ShunshunL <shunshunlyu@protonmail.com>
2020-03-13 23:08:15 -0700

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,18 @@
 class FundingRound
+  attr_reader :startup, :venture_capitalist
+  attr_accessor :type, :investment
 
+  @@all = []
+
+  def initialize(startup, venture_capitalist, type, investment)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    investment.positive?() ? @investment = investment : @investment = 0
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,17 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+startup1 = Startup.new("foo", "bar", "www.foobar.com")
+startup2 = Startup.new("baz", "qux", "www.bazqux.com")
 
+Startup.find_by_founder("foo")
+Startup.domains
+
+investor1 = VentureCapitalist.new("adam", 100000000000)
+investor2 = VentureCapitalist.new("eve", 50)
+
+funding1 = FundingRound.new(startup1, investor1, "Angel", 20.15)
+funding2 = FundingRound.new(startup1, investor1, "Angel", 25)
+funding2 = FundingRound.new(startup2, investor2, "Angel", -12.15)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
